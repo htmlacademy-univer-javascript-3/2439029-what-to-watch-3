ba5263775590f10852a3c06a8058ef634e880fcb
belutkautka <92267872+belutkautka@users.noreply.github.com>
2023-11-07 00:43:42 +0500

    add login router

--- src/components/main/app.tsx
+++ src/components/main/app.tsx
@@ -2,6 +2,7 @@ import MainPage from '../../pages/main.tsx';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import NotFound from '@pages/not-found/not-found.tsx';
 import MyList from '@pages/my-list/my-list.tsx';
+import SignIn from '@pages/sign-in/sign-in.tsx';
 
 
 type AppProps = {
@@ -17,6 +18,7 @@ function App(props: AppProps): JSX.Element {
       <Routes>
         <Route path={'/'} element={<MainPage {...props} />}/>
         <Route path={'/mylist'} element={<MyList/>}/>
+        <Route path={'/login'} element={<SignIn/>}/>
         <Route path={'*'} element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>);
