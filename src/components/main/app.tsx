03b94126404c70ee12b6e50560e625a84e254e5d
belutkautka <92267872+belutkautka@users.noreply.github.com>
2023-11-07 18:18:24 +0500

    add default review

--- src/components/main/app.tsx
+++ src/components/main/app.tsx
@@ -5,6 +5,7 @@ import MyList from '@pages/my-list/my-list.tsx';
 import SignIn from '@pages/sign-in/sign-in.tsx';
 import MoviePage from '@pages/movie-page/movie-page.tsx';
 import Player from '@pages/player/player.tsx';
+import AddReview from '@pages/add-review/add-review.tsx';
 import {Film} from 'types/film.ts';
 import {CurrentFilm} from 'types/current-film.ts';
 
@@ -16,7 +17,8 @@ function App(props: Film): JSX.Element {
         <Route path={'/mylist'} element={<MyList/>}/>
         <Route path={'/login'} element={<SignIn/>}/>
         <Route path={'*'} element={<NotFound/>}/>
-        <Route path={'/films/:id'} element={<MoviePage{...CurrentFilm}/>}/>
+        <Route path={'/films/:id'} element={<MoviePage {...CurrentFilm}/>}/>
+        <Route path={'/films/:id/review'} element={<AddReview/>}/>
         <Route path={'/player/:id'} element={<Player/>}/>
       </Routes>
     </BrowserRouter>);
