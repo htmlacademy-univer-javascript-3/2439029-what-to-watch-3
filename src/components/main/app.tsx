ebce8ec9263435157f10ca7675979d89dadfdf85
belutkautka <92267872+belutkautka@users.noreply.github.com>
2023-11-07 18:12:53 +0500

    add player

--- src/components/main/app.tsx
+++ src/components/main/app.tsx
@@ -4,6 +4,7 @@ import NotFound from '@pages/not-found/not-found.tsx';
 import MyList from '@pages/my-list/my-list.tsx';
 import SignIn from '@pages/sign-in/sign-in.tsx';
 import MoviePage from '@pages/movie-page/movie-page.tsx';
+import Player from '@pages/player/player.tsx';
 import {Film} from 'types/film.ts';
 import {CurrentFilm} from 'types/current-film.ts';
 
@@ -16,6 +17,7 @@ function App(props: Film): JSX.Element {
         <Route path={'/login'} element={<SignIn/>}/>
         <Route path={'*'} element={<NotFound/>}/>
         <Route path={'/films/:id'} element={<MoviePage{...CurrentFilm}/>}/>
+        <Route path={'/player/:id'} element={<Player/>}/>
       </Routes>
     </BrowserRouter>);
 }
