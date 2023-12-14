4cb55d8f6c4cf4b00a56e635888a7eb047d49c70
an.s.belousova <an.s.belousova@tinkoff.ru>
2023-12-15 02:28:27 +0500

    add auth filter

--- src/components/film-card-buttons/film-card-buttons.tsx
+++ src/components/film-card-buttons/film-card-buttons.tsx
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom';
+import {useAppSelector} from "@components/use-app/use-app.tsx";
 
 type FilmCardButtonsProps = {
   id: string;
 }
 
 function FilmCardButtons({id}: FilmCardButtonsProps) {
+  const auth = useAppSelector((state) => state.authorizationStatus);
   return (
     <div className="film-card__buttons">
       <button className="btn btn--play film-card__button" type="button">
@@ -13,14 +15,15 @@ function FilmCardButtons({id}: FilmCardButtonsProps) {
         </svg>
         <span>Play</span>
       </button>
-      <button className="btn btn--list film-card__button" type="button">
-        <svg viewBox="0 0 19 20" width="19" height="20">
-          <use xlinkHref="#add"></use>
-        </svg>
-        <span>My list</span>
-        <span className="film-card__count">9</span>
-      </button>
-      <Link to={`/films/${id}/review`} className="btn film-card__button">Add review</Link>
+      {auth ? <>
+        <button className="btn btn--list film-card__button" type="button">
+          <svg viewBox="0 0 19 20" width="19" height="20">
+            <use xlinkHref="#add"></use>
+          </svg>
+          <span>My list</span>
+          <span className="film-card__count">9</span>
+        </button>
+        <Link to={`/films/${id}/review`} className="btn film-card__button">Add review</Link></> : ''}
     </div>
   );
 }
